12c0fa185e1c47bf793cdcf9dc832c9109842a62
Sambarlasagna <jayashuriyajayaprakash@gmail.com>
2025-11-09 23:12:23 +0530

    implement dqn with pytorch

--- agent.py
+++ agent.py
@@ -1,18 +1,28 @@
 import flappy_bird_gymnasium
 import gymnasium
-env = gymnasium.make("FlappyBird-v0", render_mode="human", use_lidar=False)
+import torch
 
-obs, _ = env.reset()
-while True:
-    # Next action:
-    # (feed the observation to your agent here)
-    action = env.action_space.sample()
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+class Agent:
+    def run(self, is_training=True, render=False):
+        # env = gymnasium.make("FlappyBird-v0",render_mode="human",use_lidar=False)
+        env = gymnasium.make("CartPole-v1",render_mode="human" if render else None)
 
-    # Processing:
-    obs, reward, terminated, _, info = env.step(action)
-    
-    # Checking if the player is still alive
-    if terminated:
-        break
+        num_states = env.observation_space.shape[0]
+        num_actions = env.action_space.n
 
-env.close()
+        policy_dqn = DQN(num_states,num_actions).to_device(device)
+
+        obs, _ = env.reset()
+        while True:
+            action = env.action_space.sample()
+
+            # Processing:
+            obs, reward, terminated, _, info = env.step(action)
+            
+            # Checking if the player is still alive
+            if terminated:
+                break
+
+
+            env.close()
