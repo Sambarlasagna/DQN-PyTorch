1adbba412931cc17f3ecd14749882e478d765082
Sambarlasagna <jayashuriyajayaprakash@gmail.com>
2025-11-10 22:35:37 +0530

    implemented experience replay

--- agent.py
+++ agent.py
@@ -1,8 +1,37 @@
 import flappy_bird_gymnasium
 import gymnasium
 import torch
+from dqn import DQN
+from experience_replay import ReplayMemory
+import itertools
+import yaml
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+class Agent:
+    def __init__(self,hyperparameter_set):
+        with open("hyperparameters.yml", 'r') as file:
+            all_hyperparameter_sets = yaml.safe_load(file)
+            hyperparameters = all_hyperparameter_sets[hyperparameter_set]
+
+        self.hyperparameter_set = hyperparameter_set
+        # Hyperparameters (adjustable)
+        self.env_id             = hyperparameters['env_id']
+        self.learning_rate_a    = hyperparameters['learning_rate_a']        # learning rate (alpha)
+        self.discount_factor_g  = hyperparameters['discount_factor_g']      # discount rate (gamma)
+        self.network_sync_rate  = hyperparameters['network_sync_rate']      # number of steps the agent takes before syncing the policy and target network
+        self.replay_memory_size = hyperparameters['replay_memory_size']     # size of replay memory
+        self.mini_batch_size    = hyperparameters['mini_batch_size']        # size of the training data set sampled from the replay memory
+        self.epsilon_init       = hyperparameters['epsilon_init']           # 1 = 100% random actions
+        self.epsilon_decay      = hyperparameters['epsilon_decay']          # epsilon decay rate
+        self.epsilon_min        = hyperparameters['epsilon_min']            # minimum epsilon value
+        self.stop_on_reward     = hyperparameters['stop_on_reward']         # stop training after reaching this number of rewards
+        self.fc1_nodes          = hyperparameters['fc1_nodes']
+        self.env_make_params    = hyperparameters.get('env_make_params',{}) # Get optional environment-specific parameters, default to empty dict
+        self.enable_double_dqn  = hyperparameters['enable_double_dqn']      # double dqn on/off flag
+        self.enable_dueling_dqn = hyperparameters['enable_dueling_dqn']     # dueling dqn on/off flag
+
+
 class Agent:
     def run(self, is_training=True, render=False):
         # env = gymnasium.make("FlappyBird-v0",render_mode="human",use_lidar=False)
@@ -11,18 +40,29 @@ class Agent:
         num_states = env.observation_space.shape[0]
         num_actions = env.action_space.n
 
+        rewards_per_episode = []    
+
         policy_dqn = DQN(num_states,num_actions).to_device(device)
 
-        obs, _ = env.reset()
-        while True:
-            action = env.action_space.sample()
+        if is_training:
+            memory = ReplayMemory(self.replay_memory_size)
 
-            # Processing:
-            obs, reward, terminated, _, info = env.step(action)
-            
-            # Checking if the player is still alive
-            if terminated:
-                break
+        for episode in itertools.count():
+            state, _ = env.reset()
+            terminated = False
+            episode_reward = 0.0
 
+            while not terminated:
+                action = env.action_space.sample()
 
-            env.close()
+                # Processing:
+                new_state, reward, terminated, _, info = env.step(action)
+                
+                episode_reward += reward
+
+                if is_training:
+                    memory.append((state, action, reward, terminated))
+
+                state = new_state
+
+            rewards_per_episode.append(episode_reward)
