d254fa8978a8ad3090ac3b07ac3ccb91bf9c27a8
Sambarlasagna <jayashuriyajayaprakash@gmail.com>
2025-11-11 20:23:18 +0530

    Implemented epsilon greedy

--- agent.py
+++ agent.py
@@ -5,6 +5,7 @@ from dqn import DQN
 from experience_replay import ReplayMemory
 import itertools
 import yaml
+import random
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
@@ -14,25 +15,14 @@ class Agent:
             all_hyperparameter_sets = yaml.safe_load(file)
             hyperparameters = all_hyperparameter_sets[hyperparameter_set]
 
-        self.hyperparameter_set = hyperparameter_set
-        # Hyperparameters (adjustable)
-        self.env_id             = hyperparameters['env_id']
-        self.learning_rate_a    = hyperparameters['learning_rate_a']        # learning rate (alpha)
-        self.discount_factor_g  = hyperparameters['discount_factor_g']      # discount rate (gamma)
-        self.network_sync_rate  = hyperparameters['network_sync_rate']      # number of steps the agent takes before syncing the policy and target network
+     # number of steps the agent takes before syncing the policy and target network
         self.replay_memory_size = hyperparameters['replay_memory_size']     # size of replay memory
         self.mini_batch_size    = hyperparameters['mini_batch_size']        # size of the training data set sampled from the replay memory
         self.epsilon_init       = hyperparameters['epsilon_init']           # 1 = 100% random actions
         self.epsilon_decay      = hyperparameters['epsilon_decay']          # epsilon decay rate
         self.epsilon_min        = hyperparameters['epsilon_min']            # minimum epsilon value
-        self.stop_on_reward     = hyperparameters['stop_on_reward']         # stop training after reaching this number of rewards
-        self.fc1_nodes          = hyperparameters['fc1_nodes']
-        self.env_make_params    = hyperparameters.get('env_make_params',{}) # Get optional environment-specific parameters, default to empty dict
-        self.enable_double_dqn  = hyperparameters['enable_double_dqn']      # double dqn on/off flag
-        self.enable_dueling_dqn = hyperparameters['enable_dueling_dqn']     # dueling dqn on/off flag
 
 
-class Agent:
     def run(self, is_training=True, render=False):
         # env = gymnasium.make("FlappyBird-v0",render_mode="human",use_lidar=False)
         env = gymnasium.make("CartPole-v1",render_mode="human" if render else None)
@@ -40,29 +30,48 @@ class Agent:
         num_states = env.observation_space.shape[0]
         num_actions = env.action_space.n
 
-        rewards_per_episode = []    
+        rewards_per_episode = []   
+        epsilon_history = []
 
-        policy_dqn = DQN(num_states,num_actions).to_device(device)
+        policy_dqn = DQN(num_states,num_actions).to(device)
 
         if is_training:
             memory = ReplayMemory(self.replay_memory_size)
 
+            epsilon = self.epsilon_init
+
         for episode in itertools.count():
             state, _ = env.reset()
+            state = torch.tensor(state,dtype=torch.float32).to(device)
+
             terminated = False
             episode_reward = 0.0
 
             while not terminated:
-                action = env.action_space.sample()
+
+                if is_training and random.random()<epsilon:
+                    action = env.action_space.sample()
+                    action = torch.tensor(action,dtype=torch.int64).to(device)
+                else:
+                    with torch.no_grad():
+                        action = policy_dqn(state.unsqueeze(dim=0)).squeeze().argmax()
 
                 # Processing:
-                new_state, reward, terminated, _, info = env.step(action)
+                new_state, reward, terminated, _, info = env.step(action.item( ))
                 
                 episode_reward += reward
 
+                new_state = torch.tensor(new_state,dtype=torch.float32).to(device)
+                reward = torch.tensor(reward,dtype=torch.float32).to(device)
                 if is_training:
                     memory.append((state, action, reward, terminated))
 
                 state = new_state
 
             rewards_per_episode.append(episode_reward)
+
+            epsilon = max(epsilon*self.epsilon_decay,self.epsilon_min) 
+            epsilon_history.append(epsilon)
+if __name__ == '__main__':
+    agent = Agent('cartpole1')
+    agent.run(is_training=True, render=True)
