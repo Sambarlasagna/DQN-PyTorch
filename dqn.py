39460c09ecf9c64c558ed3efebf87d33425f9541
Sambarlasagna <jayashuriyajayaprakash@gmail.com>
2025-12-06 18:38:43 +0530

    trained and tested the model on cartpole and flappybird

--- dqn.py
+++ dqn.py
@@ -2,24 +2,52 @@ import torch
 from torch import nn
 import torch.nn.functional as F
 
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
 class DQN(nn.Module):
-    def __init__(self,state_dim,action_dim,hidden_dim = 256):
-        super(DQN,self).__init__()
 
-        self.fc1 = nn.Linear(state_dim,hidden_dim)
-        self.fc2 = nn.Linear(hidden_dim,action_dim)
-    
-    def forward(self,x):
+    def __init__(self, state_dim, action_dim, hidden_dim=256, enable_dueling_dqn=True):
+        super(DQN, self).__init__()
+
+        self.enable_dueling_dqn=enable_dueling_dqn
+
+        self.fc1 = nn.Linear(state_dim, hidden_dim)
+
+        if self.enable_dueling_dqn:
+            # Value stream
+            self.fc_value = nn.Linear(hidden_dim, 256)
+            self.value = nn.Linear(256, 1)
+
+            # Advantages stream
+            self.fc_advantages = nn.Linear(hidden_dim, 256)
+            self.advantages = nn.Linear(256, action_dim)
+
+        else:
+            self.output = nn.Linear(hidden_dim, action_dim)
+
+    def forward(self, x):
         x = F.relu(self.fc1(x))
-        x = self.fc2(x)
-        return x
-    
+
+        if self.enable_dueling_dqn:
+            # Value calc
+            v = F.relu(self.fc_value(x))
+            V = self.value(v)
+
+            # Advantages calc
+            a = F.relu(self.fc_advantages(x))
+            A = self.advantages(a)
+
+            # Calc Q
+            Q = V + A - torch.mean(A, dim=1, keepdim=True)
+
+        else:
+            Q = self.output(x)
+
+        return Q
+
+
 if __name__ == '__main__':
     state_dim = 12
     action_dim = 2
-    net = DQN(state_dim,action_dim).to(device)
-    state = torch.randn((10,state_dim)).to(device)
+    net = DQN(state_dim, action_dim)
+    state = torch.randn(10, state_dim)
     output = net(state)
     print(output)
